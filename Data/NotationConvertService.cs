be9c5da9e6513033860749e43510d05082b9d837
vadmazz <vadmazz2013@yandex.ru>
2019-12-04 21:36:09 +0300

    Main feature finished

--- Data/NotationConvertService.cs
+++ Data/NotationConvertService.cs
@@ -2,8 +2,19 @@
 
 namespace NotationCalculator.Data
 {
-    
-
+    public enum Letters
+    {
+        A = 10,
+        B = 11,
+        C = 12,
+        D = 13,
+        E = 14,
+        F = 15,
+        G = 16,
+        H = 17,
+        I = 18,
+        J = 19
+    }
     public class NotationConvertService
     {
         public ResultWithResponse Convert(string val1, string ns1, string ns2)
@@ -11,27 +22,42 @@ namespace NotationCalculator.Data
             int alphabet1;
             int alphabet2;
             if (val1 == null || ns1 == null || ns2 == null)
-                return new ResultWithResponse("Неверные значения"!, ConverterResponse.Error);
-            if (!int.TryParse(ns1, out alphabet1) && !int.TryParse(ns2, out alphabet2))
-                return new ResultWithResponse("Не инт"!, ConverterResponse.Error);//TODO:Вызывать метод CreateAlphabet и стринг передавать в конвертер
+                return new ResultWithResponse("Неверные значения", ConverterResponse.Error);
+            
+            if (!int.TryParse(ns1, out alphabet1))
+                return new ResultWithResponse("Неверные значения", ConverterResponse.Error);
+            
+            if(!int.TryParse(ns2, out alphabet2))
+                return new ResultWithResponse("Неверные значения", ConverterResponse.Error);
+            
             try
             {
-                var quaternarySystem = new NumeralSystem(alphabet1.ToString(), val1);
-                return new ResultWithResponse(quaternarySystem.ConvertToDecimal(), ConverterResponse.Ok);
-                quaternarySystem.ConvertToDecimal();
+                var quaternarySystem = new NumeralSystem(CreateAlphabet(alphabet1), val1);
+                var aimSystem = new NumeralSystem(CreateAlphabet(alphabet2));
+                if (ns2 == "10")
+                    return new ResultWithResponse(quaternarySystem.ConvertToDecimal(), ConverterResponse.Ok);
+                
+                return new ResultWithResponse(quaternarySystem.ConvertToAny(aimSystem), ConverterResponse.Ok);
             }
-            catch (WrongNumberException e)
+            catch (WrongNumberException)
             {
-                return new ResultWithResponse("Число не принадлежит системе счисления!", ConverterResponse.Error);
+                return new ResultWithResponse("Число не принадлежит системе счисления", ConverterResponse.Error);
+            }
+            catch (FormatException)
+            {
+                return new ResultWithResponse("Неверные значения", ConverterResponse.Error);
             }
         }
 
         private string CreateAlphabet(int count)
         {
-            string res = "";
+            var res = "";
             for (int i = 0; i < count; i++)
             {
-                res += $"{i}";
+                if (i >= 10)
+                    res += $"{(Letters) i}";
+                else
+                    res += $"{i}";
             }
 
             return res;
--- Data/NumeralSystem.cs
+++ Data/NumeralSystem.cs
@@ -7,17 +7,29 @@ namespace NotationCalculator.Data
     public class NumeralSystem
     {
         private readonly List<char> _alphabet;
-        protected string Value;
-        public NumeralSystem(string alphabet, string number)
+        private Dictionary<char, int> _alphabetDictionary = new Dictionary<char, int>();
+        private string _value;
+
+        public NumeralSystem(string alphabet)
         {
-            if (alphabet != null && number != null)
+            if (alphabet != null)
+                _alphabet = new List<char>(alphabet);
+            else
+                throw new ArgumentNullException();
+            for (var i = 0; i < _alphabet.Count; i++)
             {
-                _alphabet = new List<char>();
-                foreach (var symbol in alphabet)
-                    _alphabet.Add(symbol);
+                _alphabetDictionary.Add(_alphabet[i], i);
+            }
+        }
+        
+        public NumeralSystem(string alphabet, string number)
+            : this(alphabet)
+        {
+            if (number != null)
+            {
                 if (!Contains(number))
                     throw new WrongNumberException();
-                Value = number;
+                _value = number;
             }
             else
                 throw new ArgumentNullException();
@@ -30,13 +42,49 @@ namespace NotationCalculator.Data
             var result = 0;
             var power = _alphabet.Count;
             
-            for (var i = 0; i < Value.Length; ++i)   
+            for (var i = 0; i < _value.Length; ++i)
             {
-                var val = (int)Char.GetNumericValue(Value[Value.Length - i - 1]);
+                var val = _alphabetDictionary[_value[_value.Length - i - 1]];
+                //var val = (int)Char.GetNumericValue(Value[Value.Length - i - 1]);
+                
                 result += val * (int) Math.Pow(power, i);
             }
 
             return result.ToString();
         }
+        /// <summary>
+        /// Convert Numeral System value into new Numeral System
+        /// </summary>
+        /// <param name="aimSystem">Target Numeral System instance</param>
+        /// <returns></returns>
+        public virtual string ConvertToAny(NumeralSystem aimSystem)
+        {
+            var dec = int.Parse(ConvertToDecimal());//переводим число в десятичную
+            var intnum = 0;
+            var targetPower = aimSystem._alphabet.Count;
+            var remainder = 0;
+            var result = "";// остаток
+            
+            intnum = Math.DivRem(dec, targetPower,out remainder);
+            result = $"{FindByValue(remainder, aimSystem)}";
+            
+            while (intnum >= targetPower)
+            {
+                //получаем целое число от деления
+                intnum = Math.DivRem(intnum, targetPower,out remainder);
+                result = $"{FindByValue(remainder, aimSystem)}" + result;
+            }
+            Math.DivRem(intnum, targetPower,out remainder);
+            if (remainder != 0)
+            {
+                result = $"{FindByValue(intnum, aimSystem)}" + result;
+            }
+            
+            return result;
+        }
+
+        private string FindByValue(int value, NumeralSystem sys) =>
+            sys._alphabetDictionary
+                .First(pair => pair.Value == value).Key.ToString();
     }
 }
